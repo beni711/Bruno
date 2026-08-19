6c3f4a072f195cb6ee29d48e1bfc81efe0616516
Beni <54180653+beni711@users.noreply.github.com>
2026-08-20 00:40:24 +0200

    Bruno Cache auf Version 1.8.0 erneuern

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "bruno-v29";
+const CACHE_NAME = "bruno-v30";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=1.7.0",
-  "./app.js?v=1.7.0",
-  "./game-core.js?v=1.7.0",
+  "./styles.css?v=1.8.0",
+  "./app.js?v=1.8.0",
+  "./game-core.js?v=1.8.0",
   "./manifest.webmanifest",
   "./favicon.svg",
   "./icon-192.png",
